b87f510adc19865d139b5641b0429757aad5a161
frankdew <frankdew23@gmail.com>
2016-08-31 15:09:08 +0200

    Add more css and text

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
   root 'static_pages#index'
-
   get 'static_pages/xhtml'
-
   get 'static_pages/css'
-
   get 'static_pages/js'
+  get 'static_pages/jquery'
 
 end
