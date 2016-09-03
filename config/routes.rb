3ee002129133f20c78306e5d862a67d59d8081d2
frankdew <frankdew23@gmail.com>
2016-09-03 16:07:38 +0200

    Add style to links and more css text

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get 'static_pages/xhtml'
   get 'static_pages/css'
   get 'static_pages/js'
-  get 'static_pages/jquery'
   get 'static_pages/bootstrap'
+  get 'static_pages/jquery'
   get 'static_pages/angular_js'
 end
